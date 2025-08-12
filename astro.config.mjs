c6f4b21105c5908536f1fdc29a9e9d00dcdd827d
bibek kaliraj <bibekkaliraj111@gmail.com>
2025-08-12 08:37:53 +0545

    Design: created landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-	},
-
   integrations: [react()],
+  vite: {
+    plugins: [
+      tailwindcss(), // no arguments here
+    ],
+  },
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx}",
+    "./pages/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "navy-blue": "#2196F3",
+        "accent-blue": "#007bff",
+        "accent-blue-transparent": "rgba(0, 123, 255, 0.1)",
+        "light-blue": "#e6f0fa",
+        "blue-800": "#1E40AF",
+        "gray-600": "#4B5563",
+        "gray-300": "#D1D5DB",
+        "gray-800": "#1F2937",
+        orange: "#FFA500",
+        pink: "#FFC0CB",
+      },
+    },
+  },
+  plugins: [],
+};
